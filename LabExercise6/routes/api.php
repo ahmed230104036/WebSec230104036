0c074a467ce094f6c3315782fa18a1267912f163
ahmed360-8 <ahmed230104036@sut.edu.eg>
2025-05-21 23:52:09 +0300

    API`s

--- LabExercise6/routes/api.php
+++ LabExercise6/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 }); 
+
+Route::post('login', [UsersController::class, 'login']);
+Route::get('/users', [UsersController::class, 'users'])->middleware('auth:api');
+Route::get('/logout', [UsersController::class, 'logout'])->middleware('auth:api');
 
 
 
